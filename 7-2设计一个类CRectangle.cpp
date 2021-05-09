1ea270d83083b3d68767c167bdc40c3273927c0d
EricFang <36543092+cnyvfang@users.noreply.github.com>
2021-05-09 23:16:03 +0800

    Update 7-2设计一个类CRectangle.cpp

--- 7-2设计一个类CRectangle.cpp
+++ 7-2设计一个类CRectangle.cpp
@@ -8,21 +8,21 @@ using namespace std;
 
 class CRectangle{
 	public:
-		CRectangle(double a=1,double b=1):c(a),k(b){
+		CRectangle(double a=1,double b=1):c(a),k(b){	//构造
 			
 		}
 		void show(){
-			cout<<c*2+k*2<<endl;
+			cout<<c*2+k*2<<endl;	//计算并输出周长
 		}
 	private:
-		double c;
-		double k;
+		double c;	//长
+		double k;	//宽
 };
 
 int main(){
 	double x,y;
 	cin>>x>>y;
-	if((y<50&&x<=0&&y>0)||(y<50&&x>=50&&y>0)){
+	if((y<50&&x<=0&&y>0)||(y<50&&x>=50&&y>0)){	//(注意看题目！！！)题目要求边长范围在(0,50)，下面几个if用于处理界外情况
 		CRectangle test(y);
 		test.show();
 	}
