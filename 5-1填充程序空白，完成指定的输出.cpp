1da9464e3c8ae26ed9b3df7a64940a81bba7f872
EricFang <36543092+cnyvfang@users.noreply.github.com>
2021-05-09 22:56:33 +0800

    Create 5-1填充程序空白，完成指定的输出.cpp

---
+++ 5-1填充程序空白，完成指定的输出.cpp
@@ -0,0 +1,39 @@
+//请勿直接复制进编译器运行 代码不完整
+
+using namespace std;
+class A{
+    int i;
+public:
+    A(int k=0){
+        i=k;
+    }
+    void display(){
+        cout<<"i="<<i<<endl;
+    }
+};
+int main()
+{
+    
+A *p=new A;
+//动态创建对象p
+    p->display();
+    
+delete p;
+//删除对象p
+    p=new A(8);
+    p->display();
+    delete p;
+    
+p=new A[3];
+//p指向对象数组
+    A *q=p;
+    for(int i=0;i<3;i++){
+        
+q[i].display();
+//输出对象数组数据
+    }
+    
+delete[] p;
+//删除对象数组指针p
+    return 0;
+} 
