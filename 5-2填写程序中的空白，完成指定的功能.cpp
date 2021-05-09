826be0074fadc52df108ba45d4f5b8461a48742f
EricFang <36543092+cnyvfang@users.noreply.github.com>
2021-05-09 23:20:48 +0800

    Update 5-2填写程序中的空白，完成指定的功能.cpp

--- 5-2填写程序中的空白，完成指定的功能.cpp
+++ 5-2填写程序中的空白，完成指定的功能.cpp
@@ -1,3 +1,10 @@
+//
+//不要照抄！！不要照抄！！一定要自己看懂！！
+//
+//这题我也不知道我做的对不对 只是碰巧能跑出正确答案
+//
+//如果你有更好的见解，在Issues里告诉我√
+//
 #include<iostream>
 using namespace std;
 class Point{
@@ -6,10 +13,8 @@ class Point{
 static double test;
 //定义静态变量
 public:
-    Point(double a=0,double b=0):x(a),y(b){
-        
-test=max(x,y);
-
+    Point(double a=0,double b=0):x(a),y(b){        
+        test=max(x,y);
     }
     ~Point(){
         
@@ -17,9 +22,7 @@ test=1;
 
     }
     void show(){
-        cout<<"the number of Point is "<<
-test
-<<endl;
+        cout<<"the number of Point is "<<test<<endl;
     }
 };
 double Point::test=1;
