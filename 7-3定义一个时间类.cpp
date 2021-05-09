3f4402517d8376add330045ca8c0febbc75e7596
EricFang <36543092+cnyvfang@users.noreply.github.com>
2021-05-09 23:12:31 +0800

    Update 7-3定义一个时间类.cpp

--- 7-3定义一个时间类.cpp
+++ 7-3定义一个时间类.cpp
@@ -3,27 +3,27 @@ using namespace std;
 
 class Time{
 	public:
-		Time(int a,int b,int c){
+		Time(int a,int b,int c){	//构造函数
 			h=a;
 			m=b;
 			s=c;
 		}
-		void setTime(int a,int b,int c){
+		void setTime(int a,int b,int c){	//类的函数 手动设置时间
 			h=a;
 			m=b;
 			s=c;
 		}
-		void show(){
+		void show(){	//输出时间
 			printf("%02d-%02d-%02d\n",h,m,s);
 		}
 	private:
-		int h,m,s;
+		int h,m,s;	//定义 时分秒
 };
 
 int main(){
 	int x,y,z;
-	cin>>x>>y>>z;
-	Time a(x,y,z);
-	a.show();
+	cin>>x>>y>>z;	//输入初始时间
+	Time a(x,y,z);	//新建一个Time对象
+	a.show();	//显示时间
 	return 0;
 }
